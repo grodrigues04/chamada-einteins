41e2078679024ec15322f73a27284b7a468e4e89
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-05-30 16:34:06 -0300

    feat Relation ID with name of student in a dict

---
+++ alunosDict.py
@@ -0,0 +1,12 @@
+import pandas as pd
+id_alunos = pd.read_csv('./assets/ID Alunos - Página1.csv')
+
+linhas = id_alunos.shape[0]
+
+dictId = {}
+
+for l in range(linhas):
+    dictId[id_alunos.iloc[l,0]] = id_alunos.iloc[l,1]
+
+for items in dictId:
+    print(f'{items}:{dictId[items]}')
