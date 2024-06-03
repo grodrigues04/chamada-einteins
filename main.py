5275a81b184b227b2b51646c3c9a4ba555cabe95
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-06-03 10:31:30 -0300

    fix: arrumei a função para encontrar os valores

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
-from time import sleep
-
 from alunosDict import dictId
 import json
+
 id_alunos = pd.read_csv('./assets/ID Alunos - Página1.csv')
 prod = pd.read_csv('./assets/Planilha prod - Raw (3).csv')
+debuger = pd.read_csv('test_df.csv')
 presença = {}
 presençaPorcentagem = {}
 totalDeDias = ((prod.shape[0])//236) + 1
@@ -18,11 +18,11 @@ filtro = {
     "FALSO":"FALSE"
 }
 
-
 criador_dict = {
     "TRUE":"primeira_metade",
     "FALSE":"segunda_metade"
 }
+
 def coletarDatas():
     for dataLinha in range(1,prod.shape[0],50):
         data = prod.iloc[dataLinha,3]
@@ -35,10 +35,11 @@ def coletarDatas():
         if dia not in presença:
             # print(dataLinha, f'{dia}/{mes}')
             presença[f'{dia}/{mes}'] = {}
-    # print(presença)
 coletarDatas()
+
 diasTestador = []
 presençaDf = presença.copy()
+
 def coletarPresençaAlunos():
     for linha in range(1,prod.shape[0]):
         #print(linha)
@@ -46,16 +47,25 @@ def coletarPresençaAlunos():
         dia = criado_em[8:10]
         mes = criado_em[5:7:1]
         #print(dia)
-        alunoId = prod.iloc[linha,1]
+        alunoId = str(prod.iloc[linha,1])
         alunoNome = dictId[alunoId]
         dia_atual = presença[f'{dia}/{mes}']
         #print(filtro[prod.iloc[linha, 4]])
         #print(f'{dia}/{mes}')
         diasTestador.append(f'{dia}/{mes}')
+        valorAtualPrimeira_metade = filtro[prod.iloc[linha,4]]
+        chave = criador_dict[valorAtualPrimeira_metade]
+        if alunoId == '120':
+            print(f'{dia}/{mes}')
+            printador = (valorAtualPrimeira_metade, chave)
+            print(printador)
         if alunoNome not in presença[f'{dia}/{mes}']: #Então adiciona o aluno
             dia_atual[alunoNome] = {} #criando o dic do aluno
             alunoDict = dia_atual[alunoNome]
             alunoDict['id'] = alunoId
+            valorAtualPrimeira_metade = filtro[prod.iloc[linha,4]]
+            chave = criador_dict[valorAtualPrimeira_metade]
+            alunoDict[chave] = filtro[prod.iloc[linha,2]]
         else:
             alunoDict = dia_atual[alunoNome] #pegando apenas o dict do aluno
             valorAtualPrimeira_metade = filtro[prod.iloc[linha,4]]
@@ -80,27 +90,8 @@ with open('presença.json', 'w') as f:
 # print(presença_json)
 
 
-print('Arquivo JSON criado com sucesso!')
-
-
-#print(presença)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print('Arquivo "presença.json" criado com sucesso!')
 
 df = pd.DataFrame(presença)
 df.to_csv('Chamada Geral ultima.csv')
-print('Planilha criada com sucesso!')
+print('Planilha "Chamada Geral ultima" com sucesso!')
